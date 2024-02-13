bc6ad010d7d75f38553e0a65166b36c418c5dce3
Yifei <yifeiw701@gmail.com>
2024-02-12 20:48:26 -0500

    Revert "small updates on skills"
    This reverts commit ee184d4fe25013aadff3aad3e70aadaddc1e3c83.
    
    git push origin main

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -15,6 +15,7 @@ const Skill: React.FC<SkillProps> = ({ name, x, y }) => {
       whileHover={{ scale: 1.05 }}
       initial={{ x: 0, y: 0 }}
       whileInView={{ x: x, y: y, transition: { duration: 1.5 } }}
+      viewport={{ once: true }}
     >
       {name}
     </motion.div>
